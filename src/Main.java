d25c30b9285f88cb31b817d7f1140f1057bb9265
Lenovo <nastya.gonchuk@gmail.com>
2023-11-23 16:33:42 +0100

    Adts files

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,23 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a number");
+        int num = scanner.nextInt();
+
+        if (num > 0 ) {
+            System.out.println("Number is positive");
+        }
+        else if (num < 0) {
+            System.out.println("Number is negative");
+        }
+        else {
+            System.out.println("Number is zero");
+        }
+
+        scanner.close();
         }
     }
